ef22b1a6ba73b9590dc40b8211e3afe4b1ba2432
unknown <C:\The Bat!>
2010-08-09 23:39:13 +0400

    debug

--- www/engine/modules/mail/Mail.class.php
+++ www/engine/modules/mail/Mail.class.php
@@ -112,11 +112,17 @@ class ModuleMail extends Module {
 	//	$this->oMailer->From=$this->sFrom;
 	//	$this->oMailer->FromName=$this->sFromName;	
 
-        $this->oMailer->Host='localhost';	
-        $this->oMailer->Mailer='sendmail';
-        $this->oMailer->CharSet='utf-8';
-        $this->oMailer->From='info@1inkz.ru';
-        $this->oMailer->FromName='Блоги на Первом Казахстанском';
+$this->oMailer->Host='localhost';	
+//$this->oMailer->Port=$this->iPort;
+//$this->oMailer->Username=$this->sUsername;
+//$this->oMailer->Password=$this->sPassword;
+//$this->oMailer->SMTPAuth=$this->bSmtpAuth;
+$this->oMailer->Mailer='sendmail';
+//$this->oMailer->WordWrap=$this->iWordWrap;
+$this->oMailer->CharSet='utf-8';
+
+$this->oMailer->From='info@1inkz.ru';
+$this->oMailer->FromName='Блоги на Первом Казахстанском';
 	}
 	
 	/**
