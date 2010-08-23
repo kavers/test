92f023f20c22105ecbeb1a1cea5ad31a5ee17419
unknown <C:\The Bat!>
2010-08-23 23:43:40 +0400

    debug

--- www/classes/blocks/BlockStream.class.php
+++ www/classes/blocks/BlockStream.class.php
@@ -23,6 +23,7 @@ class BlockStream extends Block {
 	public function Exec() {
 		if ($aComments=$this->Comment_GetCommentsOnline('topic',Config::Get('block.stream.row'))) {
 			$this->Viewer_Assign('aComments',$aComments);
+pr($aComments);
 			$sTextResult=$this->Viewer_Fetch("block.stream_comment.tpl");
 			$this->Viewer_Assign('sStreamComments',$sTextResult);
 		}
